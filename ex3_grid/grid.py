4b270c8ffa6b591dd1cd987fce5b365af8ad7846
thegeekyone <x3m34life@gmail.com>
2016-12-09 23:21:34 -0500

    A sample Application that makes grid based on your number input

--- ex3_grid/grid.py
+++ ex3_grid/grid.py
@@ -1,3 +1,36 @@
+"""
+    ThinkPython2 Solution for Chapter 3 Functions
+
+    Question:
+        Write a function that draws a grid like the following:
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+|         |         |
+|         |         |
+|         |         |
+|         |         |
++ - - - - + - - - - +
+
+Hint: to print more than one value on a line,
+you can print a comma-separated sequence of values:
+print('+', '-')
+By default, print advances to the next line,
+but you can override that behavior and put a space at the end, like this:
+print('+', end=' ')
+print('-')
+The output of these statements is '+ -'.
+A print statement with no argument ends the current line and goes to the next line.
+
+Write a function that draws a similar grid with four rows and four columns.
+
+"""
+
+
+
 def print_horizontal(n):
     print('+', n*'-', end=' ')
 
@@ -21,9 +54,9 @@ def print_result(num):
 
 while True:
     try:
-        x = int(input("Please enter your number: "))
+        x = int(input("Please enter your number: ")) #type 4 if you want to make a 4x4 grid or any other number
     except ValueError:
-        print("Sorry, but I've told you to enter a number not a string: ")
+        print("Sorry, Your input is invaild please enter your number again: ")
         continue
     if x < 0:
         print("Sorry, your response must not be negative.")
